cb63ac87efbfa993dfc5d0e762c499128373260a
negin-mgdm <negin.mgdm@gmail.com>
2024-04-05 23:18:46 +0100

    feat: Creating a table and adding tasks to the database.

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import sqlite3
 
-db = sqlite3.connect('ToDoList')
 
-tasks = []
+def add_tasks(tasks):
+    db = sqlite3.connect("ToDoList")
+    cursor = db.cursor()
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS Tasks(id INTEGER PRIMARY KEY, task TEXT)
+                ''')
+    cursor.executemany(''' INSERT INTO Tasks(task) VALUES(?)''', tasks)
+    db.commit()
+    db.close()
 
 
-while True:
-    end_command = input(
-        "Please enter 'end' if you completed your To Do List: ")
-    if end_command == "end":
-        break
-    task = input("Please add a task to the To Do List: ")
-    tasks.append(task)
+def main():
+    tasks = []
+    while True:
+        task = input(
+            "Please add a task to the To Do List (enter 'end' to finish): ")
+        if task == "end":
+            break
+        tasks.append((task,))
 
-print(tasks)
+    add_tasks(tasks)
 
-delete_tasks = input(
-    "Please enter the name of the task that you wish to remove from the To Do List: ")
+
+main()
+
+# def end_tasks():
+# end_command = input(
+#     "Please enter 'end' if you completed your To Do List: ")
+
+# db = sqlite3.connect("ToDoList")
+# cursor = db.cursor()
+
+# cursor.execute('''
+#                SELECT COUNT(*) FROM Tasks Where remove_task=?, (remove_task,)
+#                ''')
+
+# delete_tasks = input(
+# "Please enter the name of the task that you wish to remove from the To Do List: ")
