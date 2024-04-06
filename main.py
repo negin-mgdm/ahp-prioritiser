bf00101c8a36b8eada3cc37d4fc213bbda8d0521
negin-mgdm <negin.mgdm@gmail.com>
2024-04-06 23:02:14 +0100

    feat: Adding a new feature to prioritise tasks using AHP method.

--- main.py
+++ main.py
@@ -25,6 +25,45 @@ def add_tasks():
     add_tasks_to_db(tasks)
 
 
+def get_tasks():
+    db = sqlite3.connect("ToDoList")
+    cursor = db.cursor()
+    cursor.execute('''
+                   SELECT * FROM Tasks''')
+    tasks = cursor.fetchall()
+    db.commit()
+    db.close()
+    return tasks
+
+
+def update_priorities(id, priority):
+    db = sqlite3.connect("ToDoList")
+    cursor = db.cursor()
+    cursor.execute('''UPDATE Tasks SET priority = ? WHERE id = ?
+                   ''', (priority, id))
+    db.commit()
+    db.close()
+
+
+def prioritise():
+    tasks = get_tasks()
+    n = len(tasks)
+    priority = [0] * n
+    print("Choose which tasks are more important:")
+    for i in range(n):
+        for j in range(i + 1, n):
+            print("A: " + tasks[i][1])
+            print("B: " + tasks[j][1])
+            choice = input('A or B: ')
+            if choice == "A":
+                priority[i] = priority[i] + 1
+            else:
+                priority[j] = priority[j] + 1
+
+    for i in range(n):
+        update_priorities(tasks[i][0], priority[i])
+
+
 def main():
     text = ''' 
     1. Add tasks 
@@ -34,6 +73,8 @@ def main():
     choice = input(text)
     if choice == "1":
         add_tasks()
+    elif choice == "2":
+        prioritise()
 
 
 main()
