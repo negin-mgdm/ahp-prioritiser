46300f913697c381c9c8edfb5b40c4d73d9a8677
negin-mgdm <negin.mgdm@gmail.com>
2024-04-11 00:09:48 +0100

    refactor: Encapsulating tasks service and database capabilities into classes.

--- main.py
+++ main.py
@@ -1,91 +1,102 @@
 import sqlite3
+import datetime
 
 
-def add_tasks_to_db(tasks):
-    db = sqlite3.connect("ToDoList")
-    cursor = db.cursor()
-    cursor.execute('''
-                CREATE TABLE IF NOT EXISTS Tasks(id INTEGER PRIMARY KEY, task TEXT, priority INTEGER)
-                ''')
-    cursor.executemany(
-        ''' INSERT INTO Tasks(task, priority) VALUES(?, ?)''', tasks,)
-    db.commit()
-    db.close()
+class TaskDbContext:
+
+    def add_tasks_to_db(self, tasks):
+        db = sqlite3.connect("ToDoList")
+        cursor = db.cursor()
+        cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS Tasks(id INTEGER PRIMARY KEY, task TEXT, priority INTEGER, "due date" TEXT)
+                    ''')
+        cursor.executemany(
+            ''' INSERT INTO Tasks(task, priority, "due date") VALUES(?, ?, ?)''', tasks,)
+        db.commit()
+        db.close()
+
+    def get_tasks_from_db(self):
+        db = sqlite3.connect("ToDoList")
+        cursor = db.cursor()
+        cursor.execute('''
+                    SELECT * FROM Tasks''')
+        tasks = cursor.fetchall()
+        db.commit()
+        db.close()
+        return tasks
+
+    def update_priorities(self, id, priority):
+        db = sqlite3.connect("ToDoList")
+        cursor = db.cursor()
+        cursor.execute('''UPDATE Tasks SET priority = ? WHERE id = ?
+                    ''', (priority, id))
+        db.commit()
+        db.close()
+
+    def remove_tasks_from_db(self, id):
+        db = sqlite3.connect("ToDoList")
+        cursor = db.cursor()
+        cursor.execute('''DELETE FROM Tasks WHERE id = ?
+                    ''', (id,))
+        db.commit()
+        db.close()
 
 
-def add_tasks():
-    tasks = []
-    while True:
-        task = input(
-            "Please add a task to the To Do List (enter 'end' to finish): ")
-        if task == "end":
-            break
-        tasks.append((task, 0,))
+class TaskService:
 
-    add_tasks_to_db(tasks)
+    db_context = TaskDbContext()
 
+    def add_tasks(self):
+        tasks = []
+        while True:
+            task = input(
+                "Please add a task to the To Do List (enter 'end' to finish): ")
+            if task == "end":
+                break
+            tasks.append((task, 0, "n/a",))
 
-def get_tasks():
-    db = sqlite3.connect("ToDoList")
-    cursor = db.cursor()
-    cursor.execute('''
-                   SELECT * FROM Tasks''')
-    tasks = cursor.fetchall()
-    db.commit()
-    db.close()
-    return tasks
+        self.db_context.add_tasks_to_db(tasks)
 
+    def prioritise(self):
+        tasks = self.db_context.get_tasks_from_db()
+        n = len(tasks)
+        priority = [0] * n
+        print("Choose which tasks are more important:")
+        for i in range(n):
+            for j in range(i + 1, n):
+                print("A: " + tasks[i][1])
+                print("B: " + tasks[j][1])
+                choice = input('A or B: ')
+                if choice == "A":
+                    priority[i] = priority[i] + 1
+                else:
+                    priority[j] = priority[j] + 1
 
-def update_priorities(id, priority):
-    db = sqlite3.connect("ToDoList")
-    cursor = db.cursor()
-    cursor.execute('''UPDATE Tasks SET priority = ? WHERE id = ?
-                   ''', (priority, id))
-    db.commit()
-    db.close()
+        for i in range(n):
+            self.db_context.update_priorities(tasks[i][0], priority[i])
 
+    def add_due_date(self):
+        tasks = self.db_context.get_tasks_from_db()
+        n = len(tasks)
+        due_date = []
+        due_date = input("Please enter a due date (DD-MM-YYYY format): ")
+        formatted_date = due_date.strftime("%d-%m-%Y")
 
-def prioritise():
-    tasks = get_tasks()
-    n = len(tasks)
-    priority = [0] * n
-    print("Choose which tasks are more important:")
-    for i in range(n):
-        for j in range(i + 1, n):
-            print("A: " + tasks[i][1])
-            print("B: " + tasks[j][1])
-            choice = input('A or B: ')
-            if choice == "A":
-                priority[i] = priority[i] + 1
-            else:
-                priority[j] = priority[j] + 1
+    def view_tasks(self):
+        tasks = self.db_context.get_tasks_from_db()
+        sorted_tasks = sorted(tasks, key=lambda task: task[2], reverse=True)
+        for task in sorted_tasks:
+            print(f"{task[1]}")
 
-    for i in range(n):
-        update_priorities(tasks[i][0], priority[i])
+    def remove_tasks(self):
+        tasks = self.db_context.get_tasks_from_db()
+        for task in tasks:
+            print(f"{task[0]}: {task[1]}")
 
-
-def view_tasks():
-    tasks = get_tasks()
-    sorted_tasks = sorted(tasks, key=lambda task: task[2], reverse=True)
-    for task in sorted_tasks:
-        print(f"{task[1]}")
-
-
-def remove_tasks():
-
-    tasks = get_tasks()
-    for task in tasks:
-        print(f"{task[0]}: {task[1]}")
-
-    choice = input("Please enter the id of the task that you wish to remove: ")
-    id = int(choice)
-
-    db = sqlite3.connect("ToDoList")
-    cursor = db.cursor()
-    cursor.execute('''DELETE FROM Tasks WHERE id = ?
-                   ''', (id,))
-    db.commit()
-    db.close()
+        choice = input(
+            "Please enter the id of the task that you wish to remove: ")
+        id = int(choice)
+        self.db_context.remove_tasks_from_db(id)
 
 
 def main():
@@ -97,15 +108,16 @@ def main():
         4. Remove tasks
         5. Exit
         '''
+        task_service = TaskService()
         choice = input(text)
         if choice == "1":
-            add_tasks()
+            task_service.add_tasks()
         elif choice == "2":
-            prioritise()
+            task_service.prioritise()
         elif choice == "3":
-            view_tasks()
+            task_service.view_tasks()
         elif choice == "4":
-            remove_tasks()
+            task_service.remove_tasks()
         elif choice == "5":
             break
 
