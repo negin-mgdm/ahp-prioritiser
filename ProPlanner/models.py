73b18067374ab5f26b3c6a9c9b53ae23acb6affc
negin-mgdm <negin.mgdm@gmail.com>
2024-04-16 23:32:16 +0100

    feat: Changing type of due_date to DateTime.

--- ProPlanner/models.py
+++ ProPlanner/models.py
@@ -4,7 +4,10 @@ from django.utils import timezone
 
 class Tasks(models.Model):
     task = models.CharField(max_length=1000)
-    due_date = models.DateField()
+    due_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.task
 
 
 task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
