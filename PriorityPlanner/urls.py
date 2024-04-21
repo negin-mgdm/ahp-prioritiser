eb391ccb71f57aa71ea973ef4154064ae6eb54ba
negin-mgdm <negin.mgdm@gmail.com>
2024-04-21 22:59:25 +0100

    feat: Adding a homepage to the ProPlanner web application.

--- PriorityPlanner/urls.py
+++ PriorityPlanner/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('ProPlanner/', include('ProPlanner.urls'))
 ]
