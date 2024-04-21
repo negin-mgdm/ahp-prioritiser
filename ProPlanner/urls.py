40e0e78196e10bd535715af567862d02a9bb2b27
negin-mgdm <negin.mgdm@gmail.com>
2024-04-21 22:44:04 +0100

    feat: Adding new details view.

--- ProPlanner/urls.py
+++ ProPlanner/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'tasks'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:task_id>', views.detail, name='detail')
+
 ]
--- ProPlanner/views.py
+++ ProPlanner/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tasks
 
 # Create your views here.
@@ -8,3 +8,8 @@ from .models import Tasks
 def index(request):
     tasks = Tasks.objects.all()
     return render(request, 'proplanner/index.html', {'tasks': tasks})
+
+
+def detail(request, task_id):
+    task = get_object_or_404(Tasks, id=task_id)
+    return render(request, 'proplanner/detail.html', {'task': task})
