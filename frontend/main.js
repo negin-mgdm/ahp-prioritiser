afd3fcbeab343b6c6bb69ac25c3e84c7a18e2ae5
negin-mgdm <negin.mgdm@gmail.com>
2024-07-31 22:32:31 +0100

    feat: Adding functionality to highlight selected item in CSS and JavaScript.

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,21 @@
 let items = ["Cooking", "Studying", "Reading", "Walking", "Dancing"];
 let ul = document.getElementById("itemsList");
+let currentlyHighlighted = null;
 for (let i = 0; i < items.length; i++) {
     let li = document.createElement("li");
     li.textContent = items[i];
+    li.addEventListener("click", function () {
+        if (li === currentlyHighlighted) {
+            li.classList.remove("highlighted");
+            currentlyHighlighted = null;
+        } else {
+            if (currentlyHighlighted) {
+                currentlyHighlighted.classList.remove("highlighted");
+            }
+            li.classList.add("highlighted");
+            currentlyHighlighted = li;
+        }
+    });
     ul.appendChild(li);
 }
+
