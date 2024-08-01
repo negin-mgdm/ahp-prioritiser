f52009c5c8ccafa9b49755b4e8ae687c427b2e77
negin-mgdm <negin.mgdm@gmail.com>
2024-08-01 23:21:16 +0100

    feat: Implementing dragging feature with real-time item shuffling.

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,20 @@
 let items = ["Cooking", "Studying", "Reading", "Walking", "Dancing"];
+
 let ul = document.getElementById("itemsList");
+
 let currentlyHighlighted = null;
+let draggedItem = null;
+
 for (let i = 0; i < items.length; i++) {
     let li = document.createElement("li");
     li.textContent = items[i];
+    li.setAttribute('draggable', 'true');
+
     li.addEventListener("click", handleItemClick);
+    li.addEventListener('dragstart', handleDragStart);
+    li.addEventListener('dragover', handleDragOver);
+    li.addEventListener('dragend', handleDragEnd);
+
     ul.appendChild(li);
 }
 
@@ -20,4 +30,40 @@ function handleItemClick(event) {
         li.classList.add("highlighted");
         currentlyHighlighted = li;
     }
+}
+
+function handleDragStart(event) {
+    draggedItem = event.target;
+    event.target.classList.add('dragging');
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', event.target.innerHTML);
+
+    let img = new Image();
+    img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/gnq0zIAAAAASUVORK5CYII=';
+    event.dataTransfer.setDragImage(img, 0, 0);
+}
+
+function handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+
+    const draggingOver = event.target;
+    if (draggingOver && draggingOver.nodeName === "LI" && draggingOver !== draggedItem) {
+        const bounding = draggingOver.getBoundingClientRect();
+        const offset = bounding.y + (bounding.height / 2);
+
+        if (event.clientY - offset > 0) {
+            draggingOver.parentNode.insertBefore(draggedItem, draggingOver.nextSibling);
+        } else {
+            draggingOver.parentNode.insertBefore(draggedItem, draggingOver);
+        }
+    }
+    return false;
+}
+
+function handleDragEnd(event) {
+    let items = document.querySelectorAll('li');
+    items.forEach(function (item) {
+        item.classList.remove('dragging');
+    });
 }
