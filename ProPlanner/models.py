068c1ad3714a96b1fec51320fff3c3d2fba81ad4
negin-mgdm <negin.mgdm@gmail.com>
2024-08-08 23:41:38 +0100

    feat: Adding item data set and randomisation of data upon page load.

--- ProPlanner/models.py
+++ ProPlanner/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=100)
     score = models.IntegerField(default=0)
+    category = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
--- ProPlanner/views.py
+++ ProPlanner/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+import random
 from django.http import JsonResponse
+from django.shortcuts import render
 from .models import Item
 
 
@@ -8,5 +9,20 @@ def index(request):
 
 
 def get_items(request):
-    items = list(Item.objects.all().values())
-    return JsonResponse(items, safe=False)
+    categories = Item.objects.values_list('category', flat=True).distinct()
+
+    if categories:
+        selected_category = random.choice(categories)
+
+        items_in_category = Item.objects.filter(category=selected_category)
+
+        if items_in_category:
+            selected_items = random.sample(
+                list(items_in_category), min(len(items_in_category), 5))
+
+            items_data = [{'id': item.id, 'title': item.title, 'score': item.score,
+                           'category': item.category} for item in selected_items]
+
+            return JsonResponse(items_data, safe=False)
+
+    return JsonResponse([], safe=False)
