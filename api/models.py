cc7dcb2dc35aef44fc22163929a23143afd40109
negin-mgdm <negin.mgdm@gmail.com>
2024-08-08 23:12:19 +0100

    delete: Removing deprecated backend logic.

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from tastypie.resources import ModelResource
-from ProPlanner.models import Tasks
-
-
-class TaskResource(ModelResource):
-    class Meta:
-        queryset = Tasks.objects.all()
-        resource_name = 'ProPlanner'
