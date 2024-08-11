5b159fc132dfe7be7e7570e59727a04aa60955a2
negin-mgdm <negin.mgdm@gmail.com>
2024-08-11 20:36:30 +0100

    chore: Setting database config.

--- PriorityPlanner/settings.py
+++ PriorityPlanner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import django_heroku
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -79,10 +80,8 @@ WSGI_APPLICATION = 'PriorityPlanner.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
 }
 
 
